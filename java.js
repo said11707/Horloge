ede9444136a80e662a1fdc2c3f3c39581fb5109e
said11707 <saidelghazzali@users.noreply.github.com>
2020-04-21 11:41:56 +0000

    Horloge

---
+++ java.js
@@ -0,0 +1,27 @@
+
+
+function currentTime() {
+    var date = new Date(); 
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+
+    hour = updateTime(hour);
+    min = updateTime(min);
+    sec = updateTime(sec);
+    document.getElementById("clock").innerText = 
+    hour + " : " + min + " : " + sec;
+     
+    var s = setTimeout(function(){currentTime() }, 1000); 
+  }
+  
+  
+  function updateTime(s) {
+    if (s < 10) {
+      return "0" + s;
+    }
+    else {
+      return s;
+    }
+  }
+currentTime()
